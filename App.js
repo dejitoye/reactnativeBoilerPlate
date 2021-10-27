e416fbb183260c1fd850463e183df16ef74cdb47
dejitoye <dejitoye71@gmail.com>
2021-10-27 16:19:05 -0500

    navigate

--- App.js
+++ App.js
@@ -1,58 +1,62 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react'
 import { View, Text, StyleSheet, Pressable } from 'react-native'
 import { Provider } from 'react-redux';
 import { Store } from './reduxstore/store';
-// import { Store } from './reduxstore/store';
+import ScreenA from './screens/ScreenA';
+import ScreenB from './screens/ScreenB';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
 
 const Stack = createStackNavigator()
+const Tabs = createBottomTabNavigator()
+const Taba = createMaterialBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
-function ScreenA({navigation}){
 
-  return(
-      <View style={styles.body}>
-      <Text style={styles.text}> testing abc 123 </Text>
 
-      <Pressable onPress= {()=>navigation.navigate("Screen-B")}> 
-        
-        <Text> go to B</Text> 
-        
-        </Pressable>
-    </View> 
-  )
-}
-function ScreenB(){
 
-  return(
-      <View style={styles.body}>
-      <Text style={styles.text}> testing abc 123 </Text>
-      <Pressable> 
-        
-        <Text> go to A</Text> 
-        
-        </Pressable>
-    </View> 
-  )
-}
+
 export default function App() {
   return (
     <Provider store={Store}>
 
       <NavigationContainer>
-      <Stack.Navigator>
-    
-<Stack.Screen 
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Screen-A') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Screen-B') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+<Tab.Screen 
 name="Screen-A"
 
 component={ScreenA}
 />
-<Stack.Screen 
+<Tab.Screen 
 name="Screen-B"
 
 component={ScreenB}
 />
-    </Stack.Navigator>
+    </Tab.Navigator>
     </NavigationContainer>
 
     </Provider>
