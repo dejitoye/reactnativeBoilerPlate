7a993ee803210f51fc16f19b49fdf13eb2fd9e5a
dejitoye <dejitoye71@gmail.com>
2021-10-27 14:59:00 -0500

    navigate

--- App.js
+++ App.js
@@ -1,14 +1,60 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
 import { Provider } from 'react-redux';
 import { Store } from './reduxstore/store';
 // import { Store } from './reduxstore/store';
+
+const Stack = createStackNavigator()
+
+function ScreenA({navigation}){
+
+  return(
+      <View style={styles.body}>
+      <Text style={styles.text}> testing abc 123 </Text>
+
+      <Pressable onPress= {()=>navigation.navigate("Screen-B")}> 
+        
+        <Text> go to B</Text> 
+        
+        </Pressable>
+    </View> 
+  )
+}
+function ScreenB(){
+
+  return(
+      <View style={styles.body}>
+      <Text style={styles.text}> testing abc 123 </Text>
+      <Pressable> 
+        
+        <Text> go to A</Text> 
+        
+        </Pressable>
+    </View> 
+  )
+}
 export default function App() {
   return (
     <Provider store={Store}>
-    <View style={styles.body}>
-      <Text style={styles.text}> testing abc 123 </Text>
-    </View>
+
+      <NavigationContainer>
+      <Stack.Navigator>
+    
+<Stack.Screen 
+name="Screen-A"
+
+component={ScreenA}
+/>
+<Stack.Screen 
+name="Screen-B"
+
+component={ScreenB}
+/>
+    </Stack.Navigator>
+    </NavigationContainer>
+
     </Provider>
 
   )
@@ -18,7 +64,8 @@ const styles = StyleSheet.create({
 marginTop:40,
 flex:1,
 justifyContent:"center",
-alignItems:"center"
+alignItems:"center",
+fontFamily:"DancingScript-Regular"
 
   },
 
